36236fa906f19afc2839f2d6f75b326a7fe62ff8
ricobalakit <rico@balak.it>
2022-11-27 19:48:34 -0600

    telemetry working for the first time

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,27 @@ void setup()
     Wire.begin();
     SetupDisplay();
     SetupBLE();
-    SetupIMU();
-    //SetupAccelerometer();
+    //SetupIMU();
+    SetupAccelerometer();
     SetupVoltageReader();
     SetupMotors();
+    SetupWeaponTelemetryConnection();
 }
 
 void loop()
 {
-    GetIMUData();
-    //GetAccelerometerData();
+    motorOutputsSetDuringTelemetryReading = false;
+    
+    //GetIMUData();
+    GetAccelerometerData();
     GetVoltageData();
+    GetWeaponTelemetry();
     DisplayText(0.01, _line0, _line1, _line2, _line3, _line4, _line5);
     SetDataForBroadcast();
-    SetMotorOutputs();
+    if(!motorOutputsSetDuringTelemetryReading)
+    {
+        SetMotorOutputs();
+    }
 }
 
 // setup
@@ -34,6 +41,8 @@ void SetupDisplay()
         return;
     }
 
+    delay(100);
+
     Display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     DisplayText(1, "DCv2 Prototype");
 
@@ -43,49 +52,55 @@ void SetupDisplay()
     _line3 = "";
     _line4 = "";
     _line5 = "";
-
-    delay(100);
 }
 
 void SetupIMU()
 {
-    if (!bno.begin())
+    delay(100);
+
+    if (!bno.begin_I2C())
     {
         DisplayText(1, "IMU Error", "Bad Init");
     }
     else
     {
         DisplayText(0.25, "IMU Success");
+        bno.enableReport(SH2_GAME_ROTATION_VECTOR);
     }
 }
 
 void SetupAccelerometer()
 {
-    if (!lis.begin_I2C(0x19))
+    delay(100);
+
+    if (!lis.begin_I2C())
     {
         DisplayText(1, "Accel Error", "Bad Init");
     }
     else
     {
         DisplayText(0.25, "Accel Success");
+        lis.setRange(H3LIS331_RANGE_400_G);
     }
 }
 
 void SetupBLE()
 {
+    delay(100);
+
     NimBLEDevice::init(BLEName);
     pServer = NimBLEDevice::createServer();
-    
+
     NimBLEService *pService = pServer->createService(SERVICE_UUID);
-    
-    
+
+
     uint16_t conn_handle = 0xFFFE;
     uint16_t minInterval = 1;
     uint16_t maxInterval = 10;
     uint16_t latency = 1000;
     uint16_t timeout = 1000;
     pServer->updateConnParams(conn_handle, minInterval, maxInterval, latency, timeout);
-    
+
     NimBLECharacteristic *pOrientationAll = pService->createCharacteristic(
             ORIENTATION_ALL_UUID,
             NIMBLE_PROPERTY::READ |
@@ -117,15 +132,17 @@ void SetupBLE()
 
 void SetupMotors()
 {
-    DisplayText(0.25, "Attaching motors", "Pins: " + String(PIN_TEST_SERVO_L) + ", " + String(PIN_TEST_SERVO_R) );
+    delay(100);
+
+    DisplayText(0.25, "Attaching motors", "Pins: " + String(PIN_TEST_SERVO_L) + ", " + String(PIN_TEST_SERVO_R));
 
     ESP32_ISR_Servos.useTimer(USE_ESP32_TIMER_NO);
-    
-    servoLIndex = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_L, 1000, 2000);
-    servoRIndex = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_R, 1000, 2000);
-    servoW0Index = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_W0, 1000, 2000);
-    servoW1Index = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_W1, 1000, 2000);
-    
+
+    servoLIndex = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_L, 0, 2000);
+    servoRIndex = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_R, 0, 2000);
+    servoW0Index = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_W0, 0, 2000);
+    servoW1Index = ESP32_ISR_Servos.setupServo(PIN_TEST_SERVO_W1, 0, 2000);
+
     if (servoLIndex == -1)
     {
         DisplayText(1, "Failure L Servo");
@@ -164,6 +181,7 @@ void SetupMotors()
 
 }
 
+
 void SetupVoltageReader()
 {
     pinMode(PIN_VOLTAGE_READER, INPUT);
@@ -173,36 +191,172 @@ void SetupVoltageReader()
 
 void GetIMUData()
 {
-    sensors_event_t event;
-    bno.getEvent(&event);
+    sh2_SensorValue_t sensorValues;
+    bno.getSensorEvent(&sensorValues);
 
     uint8_t system, gyro, accel, mag;
     system = gyro = accel = mag = 0;
-    bno.getCalibration(&system, &gyro, &accel, &mag);
 
     _line0 = "Orientation";
 
 
     _line1 =
-            "(" + String(event.orientation.x) + "," + String(event.orientation.y) + "," +
-            String(event.orientation.z) + ")";
-    orientationX = event.orientation.x;
-    orientationY = event.orientation.y;
-    orientationZ = event.orientation.z;
+            "(" + String(sensorValues.un.gameRotationVector.i) + "," + String(sensorValues.un.gameRotationVector.j) +
+            "," +
+            String(sensorValues.un.gameRotationVector.k) + "," + String(sensorValues.un.gameRotationVector.real) + ")";
+    orientationX = sensorValues.un.gameRotationVector.i;
+    orientationY = sensorValues.un.gameRotationVector.j;
+    orientationZ = sensorValues.un.gameRotationVector.k;
 
 
 }
 
 void GetAccelerometerData()
 {
+    sensors_event_t event;
+    lis.getEvent(&event);
 
+    _line0 = "Acceleration";
+    _line1 =
+            "(" + String(event.acceleration.x) + "," + String(event.acceleration.x) +
+            "," +
+            String(event.acceleration.x) + ")";
 }
 
 void GetVoltageData()
 {
     voltageReadingRaw = analogRead(PIN_VOLTAGE_READER);
     // batt is 15.38, v after divider is 1.392, raw is
-    voltageReadingMv = (short)((float)(voltageReadingRaw) * (7.907455));
+    voltageReadingMv = (short) ((float) (voltageReadingRaw) * (7.907455));
+}
+
+void SetupWeaponTelemetryConnection()
+{
+    Serial0.begin(115200); // open seria0 for serial monitor
+
+    ulong telemetryRequestWidth = 55;
+
+    ESP32_ISR_Servos.setPulseWidth(servoW0Index, telemetryRequestWidth);
+}
+
+void GetWeaponTelemetry()
+{
+    //read in telemetry from serial
+    
+    ulong microsAtStart = micros();
+    
+    ulong telemetryRequestWidth = 50;
+
+    ESP32_ISR_Servos.setPulseWidth(servoW0Index, telemetryRequestWidth);
+    
+    delay(20);
+    
+    SetMotorOutputs();
+    motorOutputsSetDuringTelemetryReading = true;
+    
+    isReadingTelemetry = true;
+
+    
+    
+    ulong microsAfterSendSignal = micros();
+    
+    while ((micros()-microsAfterSendSignal < 20000) && receivedBytes < 10)
+    {
+        if(Serial0.available())
+        {
+            SerialBuf[receivedBytes] = Serial0.read();
+            receivedBytes++;
+        }
+
+    }
+
+    ulong microsAfterReceiveSignal = micros();
+    
+    if (receivedBytes >= 9)
+    {
+        uint8_t crc8 = get_crc8(SerialBuf, 9); // get the 8 bit CRC
+
+        if (crc8 != SerialBuf[9])
+        {
+            //DisplayText(0.1f, "failed read", "corrupt data");
+            _line5 = "corrupt read";
+        }
+        else
+        {
+            ESC_telemetrie[0] = SerialBuf[0]; // temperature
+            ESC_telemetrie[1] = (SerialBuf[1] << 8) | SerialBuf[2]; // voltage
+            ESC_telemetrie[2] = (SerialBuf[3] << 8) | SerialBuf[4]; // Current
+            ESC_telemetrie[3] = (SerialBuf[5] << 8) | SerialBuf[6]; // used mA/h
+            ESC_telemetrie[4] = (SerialBuf[7] << 8) | SerialBuf[8]; // eRpM *100
+
+            W0_Temperature = (float)ESC_telemetrie[0];
+            W0_Voltage = (float)ESC_telemetrie[1]/100;
+            W0_Current = (float)ESC_telemetrie[2]/100;
+            W0_UsedMah = (float)ESC_telemetrie[3];
+            W0_Rpm = (float)ESC_telemetrie[4]*1000;
+
+            //DisplayText(0.1f, "temp: " + String(ESC_telemetrie[0]), "used mA/h: " + String(ESC_telemetrie[3]));
+            _line5 = "T:" + String(ESC_telemetrie[0]) + "C, RPM:"+String(ESC_telemetrie[4]);
+        }
+    }
+    else
+    {
+        //DisplayText(0.1f, "failed read", "not enough bytes");
+        _line5 = "no read, bytes: " + String(receivedBytes);
+    }
+
+    ulong microsAfterDecodeResponse = micros();
+    
+    receivedBytes = 0;
+    
+    ulong microsAtEnd = micros();
+
+    //microsAtStart, microsAfterSendSignal, microsAfterReceiveSignal, microsAfterDecodeResponse, microsAtEnd
+
+    /*
+    DisplayText(0.01, "Total: " + String((microsAtEnd-microsAtStart)/1000) + "ms",
+                "Set Sig: " + String((microsAfterSendSignal-microsAtStart)/1000) + "ms",
+                "Get Sig: " + String((microsAfterReceiveSignal-microsAfterSendSignal)/1000) + "ms",
+                "Decode: " + String((microsAfterDecodeResponse-microsAfterReceiveSignal)/1000) + "ms",
+                "Reset Sig: " + String((microsAtEnd-microsAfterDecodeResponse)/1000) + "ms");
+    */
+
+    //Serial.println("Requested Telemetrie");
+    //Serial.print("Temperature (C): ");
+    //Serial.println(ESC_telemetrie[0]);
+    //Serial.print("Voltage: (V) /100: ");
+    //Serial.println(ESC_telemetrie[1]);
+    //Serial.print("Current (A) /100: ");
+    //Serial.println(ESC_telemetrie[2]);
+    //Serial.print("used mA/h: ");
+    //Serial.println(ESC_telemetrie[3]);
+    //Serial.print("eRpM *100: ");
+    //Serial.println(ESC_telemetrie[4]);
+
+
+
+
+
+
+}
+
+
+uint8_t update_crc8(uint8_t crc, uint8_t crc_seed)
+{
+    uint8_t crc_u, i;
+    crc_u = crc;
+    crc_u ^= crc_seed;
+    for (i = 0; i < 8; i++)
+    { crc_u = (crc_u & 0x80) ? 0x7 ^ (crc_u << 1) : (crc_u << 1); }
+    return (crc_u);
+}
+
+uint8_t get_crc8(uint8_t *Buf, uint8_t BufLen)
+{
+    uint8_t crc = 0, i;
+    for (i = 0; i < BufLen; i++)
+    { crc = update_crc8(Buf[i], crc); }
+    return (crc);
 }
 
 // outputs
@@ -213,30 +367,36 @@ void SetMotorOutputs()
     SetMotorOutput(_testServoL, ctrlValue0);
     SetMotorOutput(_testServoR, ctrlValue1);
      */
-    
-    if(servoLIndex != -1)
+
+
+
+    if (servoLIndex != -1)
     {
-        ESP32_ISR_Servos.setPosition(servoLIndex, ctrlValue0);
+        //ESP32_ISR_Servos.setPosition(servoLIndex, ctrlValue0);
     }
-    if(servoRIndex != -1)
+    if (servoRIndex != -1)
     {
-        ESP32_ISR_Servos.setPosition(servoRIndex, ctrlValue1);
+        //ESP32_ISR_Servos.setPosition(servoRIndex, ctrlValue1);
     }
-    if(servoW0Index != -1)
+    if (servoW0Index != -1)
     {
-        ESP32_ISR_Servos.setPosition(servoW0Index, ctrlValue2);
+        ulong neutralPosition = 1500;
+        ulong controlPosition = 1000 + ctrlValue2 * (2000 / 180);
+        //ESP32_ISR_Servos.setPosition(servoW0Index, ctrlValue2);
+        ESP32_ISR_Servos.setPulseWidth(servoW0Index, controlPosition);
     }
-    if(servoW1Index != -1)
+    if (servoW1Index != -1)
     {
-        ESP32_ISR_Servos.setPosition(servoW1Index, ctrlValue3);
+        //ESP32_ISR_Servos.setPosition(servoW1Index, ctrlValue3);
     }
 }
 
 void SetDataForBroadcast()
 {
     _line2 = "-";
-    _line3 = String(voltageReadingRaw)+ " raw, " + String((float)voltageReadingMv/(float)1000) + "V";
-    _line4 = "ctrl: " + String(ctrlValue0) + " " + String(ctrlValue1)+ " " + String(ctrlValue2)+ " " + String(ctrlValue3);
+    _line3 = String(voltageReadingRaw) + " raw, " + String((float) voltageReadingMv / (float) 1000) + "V";
+    _line4 = "ctrl: " + String(ctrlValue0) + " " + String(ctrlValue1) + " " + String(ctrlValue2) + " " +
+             String(ctrlValue3);
 
 
     if (pServer->getConnectedCount())
@@ -247,19 +407,19 @@ void SetDataForBroadcast()
             NimBLECharacteristic *pChrAll = pSvc->getCharacteristic(ORIENTATION_ALL_UUID);
             if (pChrAll)
             {
-                short xShort = (short)orientationX;
-                short yShort = (short)orientationY;
-                short zShort = (short)orientationZ;
+                short xShort = (short) orientationX;
+                short yShort = (short) orientationY;
+                short zShort = (short) orientationZ;
 
 
                 byte orientationByte[8];
-                orientationByte[0] =  xShort & 0x00ff;
+                orientationByte[0] = xShort & 0x00ff;
                 orientationByte[1] = (xShort & 0xff00) >> 8;
-                orientationByte[2] =  yShort & 0x00ff;
+                orientationByte[2] = yShort & 0x00ff;
                 orientationByte[3] = (yShort & 0xff00) >> 8;
-                orientationByte[4] =  zShort & 0x00ff;
+                orientationByte[4] = zShort & 0x00ff;
                 orientationByte[5] = (zShort & 0xff00) >> 8;
-                orientationByte[6] =  voltageReadingMv & 0x00ff;
+                orientationByte[6] = voltageReadingMv & 0x00ff;
                 orientationByte[7] = (voltageReadingMv & 0xff00) >> 8;
 
 
@@ -273,9 +433,9 @@ void SetDataForBroadcast()
             {
                 ctrlAllMessage = pCtrlL->getValue();
 
-                if(ctrlAllMessage != "PEE")
+                if (ctrlAllMessage != "PEE")
                 {
-                    uint8_t* pData = (uint8_t*)ctrlAllMessage.data();
+                    uint8_t *pData = (uint8_t *) ctrlAllMessage.data();
                     /*
                     byte ctrlByteArray[8];
                     ctrlByteArray[0] =  ctrlAsInt &   0x00000000000000ff;
--- src/main.h
+++ src/main.h
@@ -13,7 +13,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_BNO055.h>
+#include <Adafruit_BNO08x.h>
 #include <NimBLEDevice.h>
 #include <Adafruit_H3LIS331.h>
 #include <ESP32_New_ISR_Servo.h>
@@ -25,9 +25,9 @@
 std::string BLEName = "DCv2_SEEED";
 
 bool _displayEnabled = true;
-bool _debugEnabled = false;
+bool _debugEnabled = true;
 Adafruit_SSD1306 Display(-1); //-1 arg means no reset pin
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Adafruit_BNO08x bno = Adafruit_BNO08x(55);
 Adafruit_H3LIS331 lis = Adafruit_H3LIS331();
 
 String _line0, _line1, _line2, _line3, _line4, _line5;
@@ -66,6 +66,25 @@ int servoW1Index = -1;
 
 bool _interpretString = false;
 
+// copied from french blheli stuff https://www.rcgroups.com/forums/showthread.php?2555162-KISS-ESC-24A-Race-Edition-Flyduino-32bit-ESC
+
+static int16_t ESC_telemetrie[5]; // Temperature, Voltage, Current, used mAh, eRpM
+
+static int16_t W0_Temperature;
+static int16_t W0_Voltage;
+static int16_t W0_Current;
+static int16_t W0_UsedMah;
+static int16_t W0_Rpm;
+
+
+static uint16_t requestTelemetrie = 0;
+static uint16_t regularThrottleSignal = 1000;
+static uint8_t SerialBuf[10];
+static uint8_t receivedBytes = 0;
+
+bool isReadingTelemetry = false;
+bool motorOutputsSetDuringTelemetryReading = false;
+
 // setup
 
 void SetupDisplay();
@@ -78,6 +97,8 @@ void SetupBLE();
 
 void SetupMotors();
 
+void SetupWeaponTelemetryConnection();
+
 void SetupVoltageReader();
 
 // inputs
@@ -88,6 +109,11 @@ void GetAccelerometerData();
 
 void GetVoltageData();
 
+void GetWeaponTelemetry();
+
+uint8_t get_crc8(uint8_t *Buf, uint8_t BufLen);
+uint8_t update_crc8(uint8_t crc, uint8_t crc_seed);
+void receiveTelemtrie();
 // outputs
 
 void SetMotorOutputs();
